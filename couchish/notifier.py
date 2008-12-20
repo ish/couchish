e41bc9c3d26afe40f0d615cd8265ff48eb23d524
Tim Parkin <tim@attic.(none)>
2008-12-20 15:56:07 +0000

    added checks for imports etc.

--- couchish/notifier.py
+++ couchish/notifier.py
@@ -7,12 +7,22 @@ log.basicConfig(level=log.INFO, filename='/tmp/couch-updater.log')
 
 
 def notifications():
+    simplejson_imported = False
     while True:
         line = sys.stdin.readline()
         log.info('read %s'%line)
         if not line:
             raise StopIteration()
-        yield line
+        if not simplejson_imported:
+            try:
+                import simplejson
+                simplejson_imported = True
+            except ImportError:
+                yield 'Bad Import'
+        try:
+            yield simplejson.loads(line)
+        except Exception, e:
+            yield 'Failed to Parse line : "%s"'%line
 
 
 def main(args):
