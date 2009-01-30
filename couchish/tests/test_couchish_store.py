35f646743dda506c2cbcfa4bf6f9ab2343c54ae2
Tim Parkin <tim.parkin@jdi-associates.com>
2009-01-30 17:00:52 +0000

    added tests for nested refs in nested sequences and for editing multiple references in the same sequence

--- couchish/tests/test_couchish_store.py
+++ couchish/tests/test_couchish_store.py
@@ -194,4 +194,78 @@ class TestNestedRefsInSequences(unittest.TestCase):
         assert matt == {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Woodall'}
         assert book == {'model_type': 'book', 'title': 'Title', 'authors': [ {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Woodall'}}, {'nested': {'_ref': tim_id, 'last_name': 'Parkin'}}]}
 
+    def test_twoentries(self):
+        sess = self.S.session()
+        matt = {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Goodall'}
+        matt_id = sess.create(matt)
+        book = {'model_type': 'book', 'title': 'Title', 'authors':[ 
+                     {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Goodall'}},
+                     {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Goodall'}}]}
+        book_id = sess.create(book)
+        sess.flush()
 
+        sess = self.S.session()
+        matt = sess.doc_by_id(matt_id)
+        matt['last_name'] = 'Woodall'
+        sess.flush()
+
+        matt = strip_id_rev(self.db[matt_id])
+        book = strip_id_rev(self.db[book_id])
+        assert matt == {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Woodall'}
+        assert book == {'model_type': 'book', 'title': 'Title', 'authors': [ {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Woodall'}}, {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Woodall'}}]}
+
+class TestNestedRefsInNestedSequences(unittest.TestCase):
+
+
+    def setUp(self):
+        server = couchdb.Server()
+        if db_name in server:
+            del server[db_name]
+        self.db = server.create(db_name)
+        self.S = store.CouchishStore(self.db, config.Config.from_yaml(
+            dict((name,type_filename(name,'nestedrefinnestedseq')) for name in ['book', 'author']),
+            type_filename('views','nestedrefinnestedseq')
+            ))
+        self.S.sync_views()
+
+    def test_simple(self):
+        sess = self.S.session()
+        matt = {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Goodall'}
+        matt_id = sess.create(matt)
+        tim = {'model_type': 'author', 'first_name': 'Tim', 'last_name': 'Parkin'}
+        tim_id = sess.create(tim)
+        book = {'model_type': 'book', 'title': 'Title', 'people':[ {'authors':[ 
+                     {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Goodall'}},
+                     {'nested': {'_ref': tim_id, 'last_name': 'Parkin'}}]}]}
+        book_id = sess.create(book)
+        sess.flush()
+
+        sess = self.S.session()
+        matt = sess.doc_by_id(matt_id)
+        matt['last_name'] = 'Woodall'
+        sess.flush()
+
+        matt = strip_id_rev(self.db[matt_id])
+        book = strip_id_rev(self.db[book_id])
+        assert matt == {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Woodall'}
+        assert book == {'model_type': 'book', 'title': 'Title', 'people': [{'authors': [ {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Woodall'}}, {'nested': {'_ref': tim_id, 'last_name': 'Parkin'}}]}]}
+
+    def test_twoentries(self):
+        sess = self.S.session()
+        matt = {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Goodall'}
+        matt_id = sess.create(matt)
+        book = {'model_type': 'book', 'title': 'Title', 'people':[ {'authors':[ 
+                     {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Goodall'}},
+                     {'nested': {'_ref': matt_id, 'first_name': 'Matt','last_name': 'Goodall'}}]}]}
+        book_id = sess.create(book)
+        sess.flush()
+
+        sess = self.S.session()
+        matt = sess.doc_by_id(matt_id)
+        matt['last_name'] = 'Woodall'
+        sess.flush()
+
+        matt = strip_id_rev(self.db[matt_id])
+        book = strip_id_rev(self.db[book_id])
+        assert matt == {'model_type': 'author', 'first_name': 'Matt', 'last_name': 'Woodall'}
+        assert book == {'model_type': 'book', 'title': 'Title', 'people': [{'authors': [ {'nested': {'_ref': matt_id, 'first_name': 'Matt', 'last_name': 'Woodall'}}, {'nested': {'_ref': matt_id, 'first_name':'Matt','last_name': 'Woodall'}}]}]}
