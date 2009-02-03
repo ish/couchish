fe4bf546edb6d8902126c6bdc16cdc9b52a3e875
Tim Parkin <tim.parkin@jdi-associates.com>
2009-02-03 09:52:22 +0000

    added tests for file upload

--- couchish/tests/test_couchish_formish_jsonbuilder.py
+++ couchish/tests/test_couchish_formish_jsonbuilder.py
@@ -1,10 +1,67 @@
 import unittest
 from couchish.couchish_formish_jsonbuilder import build
 import yaml
+from dottedish import dotted
+import webob
+from BeautifulSoup import BeautifulSoup
+import urllib
+
 
 DATADIR = 'couchish/tests/data/%s'
 
+def build_request(formname, data):
+    d = dotted(data)
+    e = {'REQUEST_METHOD': 'POST'}
+    request = webob.Request.blank('/',environ=e)
+    fields = []
+    fields.append( ('_charset)','UTF-8') )
+    fields.append( ('__formish_form__','form') )
+    for k, v in d.dotteditems():
+        fields.append( (k,v) )
+    fields.append( ('submit','Submit') )
+    request.body = urlencode( fields )
+
+    return request
+
 class Test(unittest.TestCase):
+
+    def request(self, d):
+        r = webob.Request.blank('http://localhost/')
+        r.method = 'POST'
+        r.content_type = 'application/x-www-form-urlencoded'
+        kvpairs = []
+        for k in d.dottedkeys():
+            lastsegment = k.split('.')[-1]
+            try:
+                int(lastsegment)
+                k = '.'.join(k.split('.')[:-1])
+            except ValueError:
+                pass
+            for v in d[k]:
+                kvpairs.append( (k,v) )
+
+        r.body = urllib.urlencode(kvpairs)
+        return r
+
+    def assertRoundTrip(self, f, testdata):
+        r = self.request(f._get_request_data())
+        d = f.validate(r)
+        self.assertEquals(d, testdata)
+
+    def assertIdHasValue(self, f, id, v):
+        soup = BeautifulSoup(f())
+        self.assertEquals(soup.find(id=id)['value'],v)
+        
+    def assertIdAttrHasValue(self, f, id, attr, v):
+        soup = BeautifulSoup(f())
+        s = soup.find(id=id)
+        assert 'attr' in s
+        self.assertEquals(s['attr'],v)
+
+    def assertIdAttrHasNoValue(self, f, id, attr):
+        soup = BeautifulSoup(f())
+        s = soup.find(id=id)
+        assert 'attr' not in s
 
     def test_simple(self):
         book_definition = yaml.load( open(DATADIR%'test_couchish_book.yaml').read() )
@@ -13,9 +70,24 @@ class Test(unittest.TestCase):
         author_definition = yaml.load( open(DATADIR%'test_couchish_author.yaml').read() )
         views_definition = yaml.load( open(DATADIR%'test_couchish_views.yaml').read() )
 
-        form = build(book_definition)
-        assert form.structure.attr.attrs[1][1].refersto == 'author_name'
+        f = build(author_definition)
+        self.assertIdHasValue(f, 'form-first_name', '')
+        # Test None data
+        f = build(author_definition)
+        testdata = {'first_name': None, 'last_name': None}
+        f.defaults = testdata
+        self.assertIdHasValue(f, 'form-first_name', '')
+        self.assertRoundTrip(f, testdata)
+        # Test sample data
+        f = build(author_definition)
+        testdata = {'first_name': None, 'last_name': 'Goodall'}
+        f.defaults = testdata
+        self.assertIdHasValue(f, 'form-last_name', 'Goodall')
+        self.assertRoundTrip(f, testdata)
 
-
+    def test_fileupload(self):
+        upload_definition = yaml.load( open(DATADIR%'test_upload.yaml').read() )
+        f = build(upload_definition)
+        print f()
 
         
