5250ba652edef96c4721e40dda76283dcd17f1e8
Tim Parkin <tim.parkin@jdi-associates.com>
2009-02-15 15:58:33 +0000

    added test for couchish file based addition

--- couchish/jsonutil.py
+++ couchish/jsonutil.py
@@ -1,21 +1,64 @@
-from pollen.jsonutil import *
-from schemaish.type import File as SchemaishFile
-from couchish.type import File as CouchishFile
+from pollen import jsonutil
+from schemaish.type import File
+from dottedish import dotted
 
-def couchishfile_to_dict(obj):
-    return {'filename': obj.filename,'mimetype': obj.mimetype, 'id': obj.id}
+class CouchishFile(File):
 
-def schemaishfile_to_dict(obj):
-    return {'filename': obj.filename,'mimetype': obj.mimetype}
+    def __init__(self, file, filename, mimetype, id=None, inline=True):
+        self.file = file
+        self.filename = filename
+        self.mimetype = mimetype
+        self.id = id
+        self.inline = inline
 
-def couchishfile_from_dict(obj):
-    if id in obj:
-        return CouchishFile(None, obj['filename'], obj['mimetype'], obj['id'])
-    else:
-        return SchemaishFile(None, obj['filename'], obj['mimetype'])
+    def __repr__(self):
+        return '<couchish.jsonutil.CouchishFile file="%r" filename="%s", mimetype="%s", id="%s", inline="%s" >'%(self.file, self.filename, self.mimetype, self.id, self.inline)
 
-default_system.register_type(File, file_to_dict, file_from_dict, "file")
-decode_mapping['file'] = file_from_dict
-encode_mapping[SchemaishFile] = ('file',couchishfile_to_dict)
-encode_mapping[CouchishFile] = ('file',schemaishfile_to_dict)
+def file_to_dict(obj):
+    return {'__type__': 'file','filename': obj.filename,'mimetype': obj.mimetype, 'file':None,'id':obj.id}
 
+
+def file_from_dict(obj):
+    print 'converting from!'
+    filename = obj['filename']
+    mimetype =  obj['mimetype']
+    data = obj['file']
+    id = obj.get('id')
+    return InfomyFile(data, filename, mimetype, id)
+
+jsonutil.default_system.register_type(File, file_to_dict, file_from_dict, "file")
+jsonutil.default_system.register_type(CouchishFile, file_to_dict, file_from_dict, "file")
+jsonutil.decode_mapping['file'] = file_from_dict
+jsonutil.encode_mapping[File] = ('file',file_to_dict)
+jsonutil.encode_mapping[CouchishFile] = ('file',file_to_dict)
+
+encode_to_dict = jsonutil.encode_to_dict
+decode_from_dict = jsonutil.decode_from_dict
+
+def add_id_and_attr_to_files(data):
+    dd = dotted(data)
+    for k in dd.dottedkeys():
+        if isinstance(dd[k],File):
+            segments = k.split('.')
+            for n in xrange(1,len(segments)):
+                subpath = '.'.join(segments[:-n])
+                if '_id' in dd[subpath] and '_rev' in dd[subpath]:
+                    dd[k].doc_id = dd[subpath]['_id']
+                    dd[k].rev = dd[subpath]['_rev']
+
+    return dd.data
+
+
+def wrapdumps(obj):
+    obj = jsonutil.dumps(obj)
+    return obj
+
+def wraploads(obj):
+    obj = jsonutil.loads(obj)
+    obj = add_id_and_attr_to_files(obj)
+    return obj
+
+
+
+dumps = wrapdumps
+loads = wraploads
---
+++ couchish/store2.py
@@ -0,0 +1,252 @@
+"""
+Views we can build:
+    * by type, one view should be ok
+    * x_by_y views, from config (optional)
+    * ref and ref reversed views, one pair per relationship
+"""
+
+from couchdb.design import ViewDefinition
+
+from couchdbsession import session
+
+from dottedish import dotted
+from copy import copy
+import base64
+import uuid
+from couchish import filehandling2, errors
+
+
+class CouchishStore(object):
+
+    def __init__(self, db, config):
+        self.db = db
+        self.config = config
+
+
+    def sync_views(self):
+        for url, view in self.config.viewdata['views'].items():
+            segments = url.split('/')
+            designdoc = segments[0]
+            name = '/'.join(segments[1:])
+            view = ViewDefinition(designdoc, name, view)
+            view.get_doc(self.db)
+            view.sync(self.db)
+
+
+    def session(self):
+        """
+        Create an editing session.
+        """
+        return CouchishStoreSession(self)
+
+
+    def _view(self, view):
+        """
+        Return the full name of the view, i.e. including the CouchishStore's
+        namespace.
+        """
+        return 'couchish/%s' % (self.design_doc, view)
+
+
+
+class CouchishStoreSession(object):
+
+    def __init__(self, store):
+        self.store = store
+        self.session = session.Session(store.db,
+              pre_flush_hook=self._pre_flush_hook,
+              post_flush_hook=self._post_flush_hook)
+        self.file_additions = {}
+        self.file_deletions = {}
+
+
+    def __enter__(self):
+        """
+        "with" statement entry.
+        """
+        return self
+
+
+    def __exit__(self, type, value, traceback):
+        """
+        "with" statement exit.
+        """
+        if type is None:
+            self.flush()
+        else:
+            self.reset()
+
+
+    def create(self, doc):
+        """
+        Create a document.
+        """
+        return self.session.create(doc)
+
+
+    def delete(self, doc_or_tuple):
+        """
+        Delete the given document.
+        """
+        if isinstance(doc_or_tuple, tuple):
+            id, rev = doc_or_tuple
+            doc = {'_id': id, 'rev': rev}
+        else:
+            doc = doc_or_tuple
+        return self.session.delete(doc)
+
+
+    def get_attachment(self, id_or_doc, filename):
+        return self.session._db.get_attachment(id_or_doc, filename)
+
+
+    def put_attachment(self, doc, content, filename=None, content_type=None):
+        return self.session._db.put_attachment(doc, content,
+                                            filename=filename, content_type=content_type)
+
+
+    def delete_attachment(self, doc, filename):
+        return self.session._db.delete_attachment(doc, filename)
+
+
+    def doc_by_id(self, id):
+        """
+        Return a single document, given it's ID.
+        """
+        return self.session.get(id)
+
+
+    def doc_by_view(self, view, key):
+        results = self.session.view(view, startkey=key, endkey=key, limit=2,
+                                    include_docs=True)
+        rows = results.rows
+        if len(rows) == 0:
+            raise errors.NotFound()
+        elif len(rows) == 2:
+            raise errors.TooMany()
+        return rows[0].doc
+
+
+    def docs_by_id(self, ids, **options):
+        """
+        Generate the sequence of documents with the given ids.
+        """
+        options = dict(options)
+        options['keys'] = ids
+        options['include_docs'] = True
+        results = self.session.view('_all_docs', **options)
+        return (row.doc for row in results.rows)
+
+
+    def docs_by_type(self, type, **options):
+        """
+        Generate the sequence of docs of a given type.
+        """
+        options = dict(options)
+        options['startkey'] = type
+        options['endkey'] = type
+        options['include_docs'] = True
+        results = self.view(self.store._view(type), **options)
+        return (row.doc for row in results.rows)
+
+
+    def docs_by_view(self, view, **options):
+        options = dict(options)
+        options['include_docs'] = True
+        results = self.session.view(view, **options)
+        return (row.doc for row in results.rows)
+
+
+    def view(self, view, **options):
+        """
+        Call and return a view.
+        """
+        return self.session.view(view, **options)
+
+
+    def _pre_flush_hook(self, session, deletions, additions, changes):
+        self.file_deletions, self.file_additions = filehandling2._parse_changes_for_files(session, deletions, additions, changes)
+
+
+    def flush(self):
+        """
+        Flush the session.
+        """
+        returnvalue =  self.session.flush()
+        filehandling2._handle_separate_attachments(self.session, self.file_deletions, self.file_additions)
+        return returnvalue
+
+
+    def _post_flush_hook(self, session, deletions, additions, changes):
+
+        # Sentinel to indicate we haven't retrieved the ref view data yet.
+        NO_REF_DATA = object()
+
+        # Easy access to the config.
+        views_by_viewname =  self.store.config.viewdata['views_by_viewname']
+        viewnames_by_attribute =  self.store.config.viewdata['viewnames_by_attribute']
+        attributes_by_viewname =  self.store.config.viewdata['attributes_by_viewname']
+
+        # Updates any documents that refer to documents that have been changed.
+        for doc, actions in changes:
+            doc_type = doc['model_type']
+            edited = set('.'.join([doc_type, '.'.join(str(p) for p in action['path'])])
+                         for action in actions if action['action'] == 'edit')
+            # Build a set of all the views affected by the changed attributes.
+            views = set()
+            for attr in edited:
+                views.update(viewnames_by_attribute.get(attr, []))
+            for view in views:
+                # Lazy load the ref_data.
+                ref_data = NO_REF_DATA
+                attrs_by_type = attributes_by_viewname[view]
+                view_url = views_by_viewname[view]['url']
+                # XXX should build a full key here, but let's assume just the
+                # id for a moment.
+                ref_key = doc['_id']
+                for ref_doc in self.docs_by_view(view_url+'-rev', startkey=ref_key, endkey=ref_key):
+                    # Fetch the ref data for this ref view, if we don't already
+                    # have it.
+                    if ref_data is NO_REF_DATA:
+                        ref_data = self.view(view_url, startkey=ref_key, limit=1).rows[0].value
+                        ref_data['_ref'] = ref_key
+                    for attr in attrs_by_type[ref_doc['model_type']]:
+                        # Any of the attrs sections could be a sequence.. we need to iterate over them all to find matches.. 
+                        # e.g. we may have authors*. or metadata*.authors*
+                        self._find_and_match_nested_item(ref_doc, attr.split('.'), ref_data)
+
+
+    def reset(self):
+        """
+        Reset the session, forgetting everything it knows.
+        """
+        self.session.reset()
+
+
+    def _find_and_match_nested_item(self, ref_doc, segments, ref_data, prefix=None):
+        """
+        """
+        # Initialise of copy the prefix list, because we're about to change it.
+        if prefix is None:
+            prefix = []
+        else:
+            prefix = list(prefix)
+
+        if segments == []:
+            if ref_doc['_ref'] == ref_data['_ref']:
+                ref_doc.update(ref_data)
+        else:
+            current, segments = segments[0], segments[1:]
+            if current.endswith('*'):
+                is_seq = True
+            else:
+                is_seq = False
+            current = current.replace('*','')
+
+            prefix.append(current)
+            current_ref = ref_doc[current]
+            if is_seq:
+                for ref_doc_ref in current_ref:
+                    self._find_and_match_nested_item(ref_doc_ref, segments, ref_data, prefix)
+            else:
+                self._find_and_match_nested_item(current_ref, segments, ref_data, prefix)
