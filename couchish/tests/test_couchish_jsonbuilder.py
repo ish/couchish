1f8fd054f9900cedc49c146d45f03d291e7cf9f8
Tim Parkin <tim.parkin@jdi-associates.com>
2009-02-23 16:56:03 +0000

    fixed tests after recent changes in the last few days

--- couchish/tests/test_couchish_jsonbuilder.py
+++ couchish/tests/test_couchish_jsonbuilder.py
@@ -2,7 +2,7 @@ import unittest
 from couchish.couchish_jsonbuilder import get_views
 import yaml
 from sets import Set
-from couchish import categories
+from couchish import sync_categories
 import couchdb
 from couchdb.design import ViewDefinition
 
@@ -34,16 +34,16 @@ class Test(unittest.TestCase):
 
         models_definition = {'book': book_definition, 'author': author_definition,'post': post_definition, 'dvd': dvd_definition}
         viewdata = get_views(models_definition, views_definition)
-        assert viewdata['viewnames_by_attribute'] == {'author.first_name': Set(['author_name']), 'author.last_name': Set(['author_surname', 'author_name'])}
-        assert viewdata['attributes_by_viewname'] == {'author_surname': {'dvd': Set(['writtenby']), \
+        assert viewdata['viewnames_by_attribute'] == {'author.first_name': Set(['customdes/author_name']), 'author.last_name': Set(['customdesigndoc/author_surname', 'customdes/author_name'])}
+        assert viewdata['attributes_by_viewname'] == {'customdesigndoc/author_surname': {'dvd': Set(['writtenby']), \
                                                                          'book': Set(['coauthored'])}, \
-                                                      'author_name': {'post': Set(['author']), \
+                                                      'customdes/author_name': {'post': Set(['author']), \
                                                                       'book': Set(['writtenby'])}}
         views = viewdata['views']
-        assert simplifyjs(views['customdes/author_name']) == "function(doc){if(doc.model_type=='author'){emit(doc._id,{first_name:doc.first_name,last_name:doc.last_name})}}"
-        assert simplifyjs(views['customdes/author_name-rev']) == "function(doc){if(doc.model_type=='post'){emit(doc.author._ref,null)}if(doc.model_type=='book'){emit(doc.writtenby._ref,null)}}"
-        assert simplifyjs(views['customdesigndoc/author_surname']) == "function(doc){if(doc.model_type=='author'){emit(doc._id,{last_name:doc.last_name})}}"
-        assert simplifyjs(views['customdesigndoc/author_surname-rev']) == "function(doc){if(doc.model_type=='dvd'){emit(doc.writtenby._ref,null)}if(doc.model_type=='book'){emit(doc.coauthored._ref,null)}}"
+        assert simplifyjs(views['customdes/author_name'][0]) == "function(doc){if(doc.model_type=='author'){emit(doc._id,{first_name:doc.first_name,last_name:doc.last_name})}}"
+        assert simplifyjs(views['customdes/author_name-rev'][0]) == "function(doc){if(doc.model_type=='post'){emit(doc.author._ref,null)}if(doc.model_type=='book'){emit(doc.writtenby._ref,null)}}"
+        assert simplifyjs(views['customdesigndoc/author_surname'][0]) == "function(doc){if(doc.model_type=='author'){emit(doc._id,{last_name:doc.last_name})}}"
+        assert simplifyjs(views['customdesigndoc/author_surname-rev'][0]) == "function(doc){if(doc.model_type=='dvd'){emit(doc.writtenby._ref,null)}if(doc.model_type=='book'){emit(doc.coauthored._ref,null)}}"
 
 
     
@@ -55,12 +55,15 @@ class Test(unittest.TestCase):
         models_definition = {'author': author_definition, 'post': post_definition}
         
         viewdata = get_views(models_definition, views_definition)
-        assert simplifyjs(viewdata['views']['author/by_last_name']) == "function(doc){if(doc.model_type=='author'){emit(doc.last_name,null)}}"
-        assert simplifyjs(viewdata['views']['post/all']) == "function(doc){if(doc.model_type=='post'){emit(doc._id,null)}}"
+        print '*'*80
+        print viewdata
+        print '*'*80
+        assert simplifyjs(viewdata['views']['author/by_last_name'][0]) == "function(doc){if(doc.model_type=='author'){emit(doc.last_name,null)}}"
+        assert simplifyjs(viewdata['views']['post/all'][0]) == "function(doc){if(doc.model_type=='post'){emit(doc._id,null)}}"
 
     def test_categories_creation(self):
         categories_definition = yaml.load( open(DATADIR%'categories.yaml').read() )
-        categories.create_categories(self.db, categories_definition) 
+        sync_categories.sync(self.db, categories_definition) 
 
     def test_views_creation(self):
         book_definition = yaml.load( open(DATADIR%'test_couchish_book.yaml').read() )
@@ -71,10 +74,9 @@ class Test(unittest.TestCase):
         models_definition = {'book': book_definition, 'author': author_definition,'post': post_definition, 'dvd': dvd_definition}
         viewdata = get_views(models_definition, views_definition)
         for url, view in viewdata['views'].items():
-            segments = url[1:].split('/')
-            designdoc = segments[0]
-            name = '/'.join(segments[1:])
-            view = ViewDefinition(designdoc, name, view)
+            print 'url:',repr(url), 'view',repr(view)
+            designdoc = url.split('/')[0]
+            view = ViewDefinition(designdoc, url, view[0])
             view.get_doc(self.db)
             view.sync(self.db)
 
@@ -89,8 +91,8 @@ class Test(unittest.TestCase):
         viewdata = get_views(models_definition, views_definition)
         views = viewdata['views']
 
-        assert simplifyjs(views['couchish/author_name']) == "function(doc){if(doc.model_type=='author'){emit(doc._id,{first_name:doc.first_name,last_name:doc.last_name})}}"
-        assert simplifyjs(views['couchish/author_name-rev']) == "function(doc){if(doc.model_type=='post'){emit(doc.author._ref,null)}}"
+        assert simplifyjs(views['couchish/author_name'][0]) == "function(doc){if(doc.model_type=='author'){emit(doc._id,{first_name:doc.first_name,last_name:doc.last_name})}}"
+        assert simplifyjs(views['couchish/author_name-rev'][0]) == "function(doc){if(doc.model_type=='post'){emit(doc.author._ref,null)}}"
 
 
 
