5c171fb300c13c20b896930dbf4bed39754150be
Matt Goodall <matt.goodall@gmail.com>
2009-04-02 12:15:54 +0100

    Add required package

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(name='couchish',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          "PyYAML",
       ],
       entry_points="""
       # -*- Entry points: -*-
