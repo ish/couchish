29c31a42aa733ad5f04a3863bfecb30954666a78
Tim Parkin <tim.parkin@jdi-associates.com>
2010-03-11 16:09:18 +0000

    updated description in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.1'
+version = '0.2.2'
 
 setup(name='couchish',
       version=version,
-      description="",
+      description="Couchdb library that includes reference cacheing triggers and consolidates updates.",
       long_description="""\
+A couch wrapper that includes reference info updating, serialisation of complex types into json, filehandling (returning filehandles, storing document filehandles as attachments and many other bits). Get in touch if any of this sounds interesting.
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
